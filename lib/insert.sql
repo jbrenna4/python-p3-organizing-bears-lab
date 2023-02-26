d0b00828b24069887e83a7fd5cac175b8536d1b1
Jim Brennan <jim.j.brennan@gmail.com>
2023-02-26 16:37:43 -0500

    passed all tests

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate',1, 'M', 'brown', 'chill', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy',3, 'M', 'brown', 'chill', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha',3, 'F', 'black', 'chill', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown',4, 'M', 'brown', 'chill', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa',8, 'F', 'white', 'chill', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch',10, 'M', 'white', 'chill', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy',4, 'F', 'brown', 'chill', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL,2, 'M', 'black', 'chill', 0);
